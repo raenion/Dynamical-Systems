616b8c509da4571ed01456f90eff7ec1e2c7ed32
raenion <nkoudeih@gmail.com>
2025-12-21 21:22:49 +0000

    Update 1Ddirichlet.py

--- Heat Equation/1D/Dirichlet/1Ddirichlet.py
+++ Heat Equation/1D/Dirichlet/1Ddirichlet.py
@@ -1,43 +1,81 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+"""
+1D Heat Equation
+Domain: [-1, 1]
+BC: Dirichlet
+Scheme: Explicit Euler (FTCS)
+"""
+
+# Space:
+
+nx = 600
+dx = 2/(nx-1)
+x = np.linspace(-1, 1, nx)
+
+# Time:
+
 T = 3
 dt = 0.00001
 N = int(T/dt) + 1
-nx = 600
-dx = 2/(nx-1)
+t = np.zeros(N)
+
+# Physics:
+
 nu = 0.1
 
-x = np.linspace(-1, 1, nx)
-t = np.zeros(N)
+# Stability check and conditional adjustment:
+
+if dt > dx**2/(2*nu):
+    dt = dx**2/(2*nu)
+    N = int(T/dt)+1
+    t = np.zeros(N)
+    print(f'Stability Warning: Timestep adjusted to dt = {dt}')
+
+# Initialization:
+
 u = np.zeros((N, nx))
+
+## Some possible initial conditions:
 
 #u0 = 1/8*(x+1)**4
 #u0 = np.sin(4*x)
 #u0 = np.exp(100*x)
-u0 = np.tan(1.5*x)
-
+u0 = np.tan(1.5*x) # intentionally sharp IC to test stability
 
 u[0] = u0
+
+# Save boundary values as constants: 
 
 c1 = u0[0]
 c2 = u0[-1]
 
-def laplacian1D(vec):      # same function name as periodic 1d laplacian
+# Laplcain 
+
+def laplacian1D(vec):
+
+    # Dirichlet boundary conditions respected by keeping boundaries 0
     
     newvec = np.zeros_like(vec) 
     newvec[1:-1] = (vec[:-2] - 2*vec[1:-1] + vec[2:]) / dx**2
     
     return newvec
 
+# Explicit Euler scheme integrator loop.
+
 for i in range(N-1):
     t[i+1] = t[i] + dt
     u[i+1] = u[i] + nu*laplacian1D(u[i])*dt
-    u[i+1,0] = c1
-    u[i+1,-1] = c2
+
+    # Keeping Dirichlet condition by setting boundaries back to fixed values
+    
+    u[i+1, 0] = c1
+    u[i+1, -1] = c2
+
+# Plotting:
 
 fig, ax = plt.subplots(figsize=(12,7))
-
 
 plottimes = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.75, 1.5, 3]
 
