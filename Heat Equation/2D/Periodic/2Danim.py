c77a6b51c60512482880cd44d82f417f8081eb82
raenion <nkoudeih@gmail.com>
2025-12-24 18:27:30 +0000

    Update 2Danim.py

--- Heat Equation/2D/Periodic/2Danim.py
+++ Heat Equation/2D/Periodic/2Danim.py
@@ -1,6 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+"""
+2D Heat Equation
+Domain: [-1, 1]x[-1, 1]
+BC: Periodic
+Scheme: Explicit Euler (FTCS)
+"""
+
 # Space:
 
 nx = 50
@@ -22,21 +29,23 @@ t = np.zeros(N)
 
 mu = 0.1
 
-##
-
-
-u = np.zeros((N, ny, nx))
-
+# Some possible initial conditions:
 
 u0 = np.sin(2*np.pi*x) + np.cos(2*np.pi*y)
 #u0 = np.sin(2*np.pi*x) + 2/3*np.cos(5*np.pi*x) + np.sin(2*np.pi*y) + 2/3*np.cos(5*np.pi*y)
 #u0 = np.sin(np.pi*x*y)
 #u0 = np.sin(2*np.pi*x) + 2/3*np.cos(5*np.pi*x)
 
+# Initialization: 
+
+u = np.zeros((N, ny, nx))
 u[0,:,:] = u0
 
+# 2D Laplacian:
 
 def laplacian2D(mat):
+
+    # np.roll for periodic boundary conditions
 
     vlaplacian = (np.roll(mat, -1, axis=0) - 2*mat + np.roll(mat, 1, axis=0))/dy**2
     
@@ -44,6 +53,7 @@ def laplacian2D(mat):
 
     return vlaplacian + hlaplacian
 
+# Explicit Euler scheme integrator loop:
 
 for i in range(N-1):
 
@@ -52,11 +62,10 @@ for i in range(N-1):
     u[i+1,:,:] = u[i,:,:] + mu*laplacian2D(u[i,:,:])*dt
 
 
-###### Animate
+# Animation:
 
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib.animation import FuncAnimation
-
 
 plt.style.use('dark_background')
 
@@ -65,17 +74,17 @@ fig, ax = plt.subplots(subplot_kw={'projection': '3d'}, figsize=(12,7))
 ax.set_xlabel('x')
 ax.set_ylabel('y')
 ax.set_zlabel('u')
-
+ax.set_zlim(np.min(u[0]), np.max(u[0])) 
 
 surf = ax.plot_surface(x,y,u[0], cmap='viridis', edgecolor='none')
 
-# there are currently stutters/mini-jumps/inconsistencies in the animation, why?
+# TODO: There are currently stutters/mini-jumps/inconsistencies in the animation.
 
-def update(frames):
-    global surf                                             # so that .remove doesnt think its trying to be applied to surf that is redefined after it?
-    surf.remove()                                           # instead of ax.clear so update() doesnt have to redraw unnecessary things every time its called by funcanimation
-    surf = ax.plot_surface(x,y,u[frames], cmap='viridis', edgecolor='none')
-    ax.set_zlim(np.min(u[0]), np.max(u[0]))                                     # doesnt need to be in the loop
+def update(frame):
+    global surf
+    surf.remove()
+    surf = ax.plot_surface(x,y,u[frame], cmap='viridis', edgecolor='none')
+    
     return surf,
 
 steps = 30
