48ff4711f7207d99706389214639a31e77f7a426
raenion <nkoudeih@gmail.com>
2026-01-02 22:52:49 +0000

    update animation wobble.py

--- Planetary Orbits/animation wobble.py
+++ Planetary Orbits/animation wobble.py
@@ -2,36 +2,40 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-# ToDo: clean, sort, make planet more massive, move closer to sun, rename variables
+# ToDo: clean, sort, make planet more massive, move closer to star, rename variables
 
-# we NO LONGER assume sun is fixed, i.e. we compute gravitational pull of Earth on Sun
+# we NO LONGER assume star is fixed, i.e. we compute gravitational pull of planet on star
 # due to conservation of angular momentum orbit lies in a plane, say the xy plane
 
-T = 365*24*3600*4
-dt = 3600
+# Time:
+
+T = 365*24*3600
+dt = 360
 N = int(T/dt) + 1
 
+# Physics:
+
 G = 6.6734810e-11 # m^3 / (kg s^2)
-m_sun = 1.989e30 # kg
-m_earth = 5.972e24 # kg
-R = 1.496e11 # m   (average radius of earth's orbit around sun)
+m_star = 1.989e30 # kg
+m_planet = 1e28 # kg
+R = 1e10 # m
 
-r0_earth = [R, 0]
-v0_earth = [0, 29.8e3]
-r0_sun = [0, 0]
-v0_sun = [0, 0]
+r0_planet = [R, 0]
+v0_planet = [0, 29.8e3]
+r0_star = [0, 0]
+v0_star = [0, 0]
 
-r_earth = np.zeros((N,2))
-v_earth = np.zeros((N,2))
+r_planet = np.zeros((N,2))
+v_planet = np.zeros((N,2))
 
-r_sun = np.zeros((N,2))
-v_sun = np.zeros((N,2)) 
+r_star = np.zeros((N,2))
+v_star = np.zeros((N,2)) 
 
-r_earth[0] = r0_earth
-v_earth[0] = v0_earth
+r_planet[0] = r0_planet
+v_planet[0] = v0_planet
 
-r_sun[0] = r0_sun
-v_sun[0] = v0_sun
+r_star[0] = r0_star
+v_star[0] = v0_star
 
 norm = np.linalg.norm
 
@@ -41,25 +45,24 @@ def gravitational_acceleration(r1, r2, m2):
 
 a = gravitational_acceleration
 
+# Verlet integration scheme:
+
 for i in range(N-1):
 
-    r_earth[i+1] = r_earth[i] + v_earth[i] * dt + 1/2*a(r_earth[i], r_sun[i], m_sun) * dt**2    
-    r_sun[i+1] = r_sun[i] + v_sun[i] * dt + 1/2*a(r_sun[i], r_earth[i], m_earth) * dt**2
+    r_planet[i+1] = r_planet[i] + v_planet[i] * dt + 1/2*a(r_planet[i], r_star[i], m_star) * dt**2    
+    r_star[i+1] = r_star[i] + v_star[i] * dt + 1/2*a(r_star[i], r_planet[i], m_planet) * dt**2
 
+    v_planet[i+1] = v_planet[i] + 1/2*(a(r_planet[i], r_star[i], m_star) + a(r_planet[i+1], r_star[i+1], m_star) ) * dt
+    v_star[i+1] = v_star[i] + 1/2*( a(r_star[i], r_planet[i], m_planet) + a(r_star[i+1], r_planet[i+1], m_planet) ) * dt
 
-    v_earth[i+1] = v_earth[i] + 1/2*(a(r_earth[i], r_sun[i], m_sun) + a(r_earth[i+1], r_sun[i+1], m_sun) ) * dt
-    v_sun[i+1] = v_sun[i] + 1/2*( a(r_sun[i], r_earth[i], m_earth) + a(r_sun[i+1], r_earth[i+1], m_earth) ) * dt
-
-fig, ax = plt.subplots(figsize=(8,8))
-
-#ax.set_aspect('equal', 'box')
-
-
+# Animation: 
 
 from matplotlib.animation import FuncAnimation
 
-(line_earth,) = ax.plot([], [])
-(line_sun,) = ax.plot([], [])
+fig, ax = plt.subplots(figsize=(8,8))
+
+(line_planet,) = ax.plot([], [])
+(line_star,) = ax.plot([], [])
 
 ax.set_ylim(-1.5*R, 1.5*R)
 ax.set_xlim(-1.5*R, 1.5*R)
@@ -68,12 +71,11 @@ steps = 10
 
 frames = range(0, N, steps)
 
-
 def update(frame):
-    line_earth.set_data(r_earth[:frame, 0], r_earth[:frame, 1])
-    line_sun.set_data(r_sun[:frame, 0], r_sun[:frame, 1])
+    line_planet.set_data(r_planet[:frame, 0], r_planet[:frame, 1])
+    line_star.set_data(r_star[:frame, 0], r_star[:frame, 1])
 
-    return line_earth, line_sun
+    return line_planet, line_star
 
 
 ani = FuncAnimation(fig, update, frames=frames, interval=20)
