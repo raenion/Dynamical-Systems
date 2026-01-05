b02225d0030d365fab88da912eafaccc3eee6d7d
raenion <nkoudeih@gmail.com>
2026-01-05 23:33:59 +0000

    Update 1DWave.py

--- Wave Equation/1D/1DWave.py
+++ Wave Equation/1D/1DWave.py
@@ -1,23 +1,52 @@
+"""
+1D Wave Equation Solver using Finite Differences
+
+System: ∂²y/∂t² = c² ∂²y/∂x²
+
+Scheme: Explicit central difference in time and space
+
+Boundary conditions: Dirichlet (Fixed Ends)
+
+"""
+
+
 import numpy as np
 import matplotlib.pyplot as plt
+
+# Space:
+
+L = 10
+nx = 500
+dx = L/(nx-1)
+x = np.linspace(0, L, nx)
+
+# Time:
 
 T = 5
 dt = 0.001
 N = int(T/dt) + 1
+t = np.zeros(N)
+
+# Physics:
+
 c = 2
 
-L = 10
-nx = 500
-x = np.linspace(0, L, nx)
-dx = L/(nx-1)
+# Stability:
 
-t = np.zeros(N)
+if c * dt / dx > 1:
+    raise ValueError(f'Stability condition violated: c*dt/dx must be <= 1. Suggested: dt = {dx/c}.')
+
+# Solution initialization:
+
 y = np.zeros((N, nx))
 
+## Some initial conditions:
 
 #y0 = np.sin(np.pi*x/L)
 #y0 = np.exp(-10*(x-L/2)**2)
 y0 = np.cos((np.pi/10*(x-L/2)))
+
+## Initialization:
 
 y[0] = y0
 
@@ -25,6 +54,7 @@ y[1] = y0
 
 
 def laplacian1D(vec):
+    
     newvec = np.zeros_like(vec)
 
     newvec[1:-1] = (vec[:-2] - 2*vec[1:-1] + vec[2:])/dx**2
@@ -32,11 +62,12 @@ def laplacian1D(vec):
     return newvec
 
 for i in range(1, N-1):
+    
     t[i] = t[i-1] + dt
+    
     y[i+1] = 2*y[i] - y[i-1] + (c**2)*laplacian1D(y[i])*(dt**2)
 
-for i in range(0, N, int(N/5)):                                       # remove
-    print(y[i, int(nx/2)])
+# Plotting:
 
 fig, ax = plt.subplots()
 
